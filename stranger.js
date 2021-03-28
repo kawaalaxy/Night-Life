b6080ddd7a409aad70ab4615d10ef7956ebf1122
kawaalaxy <clementg0105@hotmail.fr>
2021-03-28 05:51:26 +0200

    removing config.json call

--- stranger.js
+++ stranger.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-const config = require('./config.json');
 const prefix = '!';
 const fs = require('fs');
 client.commands = new Discord.Collection();
