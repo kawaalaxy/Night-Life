75260b4257756c85542b85cb8c407d4d83a0b9ce
kawaalaxy <clementg0105@hotmail.fr>
2021-03-28 05:00:49 +0200

    reaction roles

---
+++ Stranger/commands/reactionrole.js
@@ -0,0 +1,70 @@
+module.exports =
+{
+  name: 'reactionrole',
+  description: 'crée un message de rôle par réaction',
+  async execute(message, args, Discord, client)
+  {
+    const channel = '825508230238568518';
+    const spiRole = message.guild.roles.cache.find(role => role.name ==='spi');
+    const cmiRole = message.guild.roles.cache.find(role => role.name ==='cmi');
+    const spiEmoji = '🙂';
+    const cmiEmoji = '🙃';
+
+     let embed = new Discord.MessageEmbed()
+     .setTitle('Choisissez votre groupe')
+     .setDescription(''
+        +`${spiEmoji} pour les spi\n`
+        +`${cmiEmoji} pour les cmi`);
+
+      let messageEmbed = await message.channel.send(embed);
+      messageEmbed.react(spiEmoji);
+      messageEmbed.react(cmiEmoji);
+
+      client.on('messageReactionAdd', async(reaction, user) =>
+    {
+      if (reaction.message.partial) await reaction.message.fetch();
+      if (reaction.partial) await reaction.fetch();
+      if(user.bot) return;
+      if (!reaction.message.guild) return;
+
+      if (reaction.message.channel.id == channel)
+      {
+        if (reaction.emoji.name === spiEmoji)
+        {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(spiRole);
+        }
+        if (reaction.emoji.name === cmiEmoji)
+        {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(cmiRole);
+        }
+      }
+      else
+      {
+        return;
+      }
+    });
+    client.on('messageReactionRemove', async(reaction, user) =>
+  {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if(user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.channel.id == channel)
+    {
+      if (reaction.emoji.name === spiEmoji)
+      {
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(spiRole);
+      }
+      if (reaction.emoji.name === cmiEmoji)
+      {
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(cmiRole);
+      }
+    }
+    else
+    {
+      return;
+    }
+  });
+  }
+}
--- Stranger/stranger.js
+++ Stranger/stranger.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const config = require('./config.json');
 const prefix = '!';
 const fs = require('fs');
@@ -38,6 +38,11 @@ client.on('message', message =>
   else if (command == 'clear')
   {
     client.commands.get('clear').execute(message, args);
+  }
+  else if (command == 'reactionrole')
+  {
+    client.commands.get('clear').execute(message, "1");
+    client.commands.get('reactionrole').execute(message, args, Discord, client);
   }
 });
 
