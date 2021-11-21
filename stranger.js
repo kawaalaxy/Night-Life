f63e5e3d749bf6caf5a164a435e97ac04727dc87
kawaalaxy <clementg0105@hotmail.fr>
2021-11-21 20:34:22 +0100

    encore

--- stranger.js
+++ stranger.js
@@ -17,7 +17,7 @@ client.once('ready', () =>
 
 client.on('message', message =>
 {
-  if(!message.content.startsWith(prefix) || /*message.author.bot*/) return;
+  if(!message.content.startsWith(prefix) /*|| message.author.bot*/) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
