806236c252c15258ff8caed19f84b05c41f95241
kawaalaxy <clementg0105@hotmail.fr>
2021-11-23 23:46:10 +0100

    parle pour moi

---
+++ commands/parle.js
@@ -0,0 +1,17 @@
+module.exports =
+{
+  name: 'parle',
+  description: 'parle pour moi',
+  async execute(message, args)
+  {
+    const modoRole = message.guild.roles.cache.find(role => role.name ==='modo');
+    if(message.member.roles.cache.has(modoRole.id))
+    {
+      message.channel.send(args);
+    }
+    else
+    {
+      return message.reply("vous n'avez pas l'autorisation de faire ceci")
+    }
+  }
+}
