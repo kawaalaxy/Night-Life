4f6454fa078591b20fd62b8ee8df132a2cbbab4a
kawaalaxy <clementg0105@hotmail.fr>
2021-12-07 19:57:57 +0100

    choix up plus besoin du log

--- commands/choix.js
+++ commands/choix.js
@@ -11,8 +11,6 @@ module.exports =
     r = Math.random()*(liste_choix.length);
     r = r - (r%1);
     liste_choix = ["Certainement pas !" , "Demande à Benji" , "N'y compte pas" , "C'est certain" , "Quel beau temps aujourd'hui, tu ne trouves pas ?" , "Demande à Clément", "Demande à ta mère" , "oui.", "Demande à Romane", "mdr xD", "Carrément", "Demande à Diane"];
-    console.log(r);
-    console.log(liste_choix[r]);
     message.channel.send(liste_choix[r]);
   }
 }
