f876418446900b6c61ddd49b03955d88fb5cb4a2
kawaalaxy <clementg0105@hotmail.fr>
2021-12-07 19:55:05 +0100

    choix up

--- commands/choix.js
+++ commands/choix.js
@@ -10,7 +10,7 @@ module.exports =
     }
     r = Math.random()*6;
     r = r - (r%1);
-    liste_choix = ["Certainement pas !", "oui.", "Demande à Romane", "mdr xD", "Carrément", "Demande à Diane"];
+    liste_choix = ["Certainement pas !" , "Demande à Benji" , "N'y compte pas" , "C'est certain" , "Quel beau temps aujourd'hui, tu ne trouves pas ?" , "Demande à Clément", "Demande à ta mère" , "oui.", "Demande à Romane", "mdr xD", "Carrément", "Demande à Diane"];
     console.log(r);
     console.log(liste_choix[r]);
     message.channel.send(liste_choix[r]);
