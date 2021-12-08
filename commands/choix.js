c8bc3a092eccf0d988d9236f3aebfabf5996386b
kawaalaxy <32813468+kawaalaxy@users.noreply.github.com>
2021-12-08 12:41:35 +0100

    choix up

--- commands/choix.js
+++ commands/choix.js
@@ -8,7 +8,7 @@ module.exports =
     {
       return message.reply("dis moi quoi choisir");
     }
-    r = Math.random()*(liste_choix.length);
+    r = Math.random()*(16);
     r = r - (r%1);
     liste_choix = ["Certainement pas !" , "Tu peux compter là dessus !" , "Très probablement" , "Je ferais mieux de ne pas te le dire maintenant..." , "Impossible !" , "Demande à Benji" , "N'y compte pas..." , "C'est certain" , "Quel beau temps aujourd'hui, tu ne trouves pas ?" , "Demande à Clément", "Demande à ta mère" , "oui.", "Demande à Romane", "mdr xD", "Carrément !", "Demande à Diane"];
     message.channel.send(liste_choix[r]);
