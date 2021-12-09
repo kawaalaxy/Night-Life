e79af4c1d8b539d37ba2048f254ce78bf29a4a46
kawaalaxy <clementg0105@hotmail.fr>
2021-12-09 19:34:54 +0100

    up tu tourne

--- stranger.js
+++ stranger.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const prefix = '!';
 const fs = require('fs');
+trn = 0;
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles)
