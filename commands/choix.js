b422a29582e70f16bf163903e409fc0bf231a358
kawaalaxy <clementg0105@hotmail.fr>
2021-12-09 05:00:32 +0100

    choix up

--- commands/choix.js
+++ commands/choix.js
@@ -10,8 +10,7 @@ module.exports =
     }
     r = Math.random()*(1);
     r = r - (r%1);
-    const args = message.content.slice(prefix.length).split(" ou ");
-    message.channel.send(agrs[r]);
-    message.channel.send(liste_choix[r]);
+    const reponse = message.content.slice(prefix.length).split(" ou ");
+    message.channel.send(reponse[r]);
   }
 }
