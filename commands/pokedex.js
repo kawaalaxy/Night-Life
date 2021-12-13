d0d2f33d52282e10ef5f7e6124f3809ac87d6d90
kawaalaxy <clementg0105@hotmail.fr>
2021-12-13 23:14:08 +0100

    ajout de pokedex.js et pokedex.txt

---
+++ commands/pokedex.js
@@ -0,0 +1,18 @@
+module.exports =
+{
+  name: 'pokédex',
+  description: 'donne un pokémon du pokédex',
+  async execute(message)
+  {
+    r = Math.random()*898;
+    r = r - (r%1) + 1;
+    fs.readFile('pokedex.txt', 'utf8', (err, data) =>{
+      if (err){
+        console.error(err);
+        return;
+      }
+      console.log(data);
+    });
+    message.channel.send("Ton pokémon est :");
+  }
+}
