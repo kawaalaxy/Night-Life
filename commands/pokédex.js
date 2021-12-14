aae0e95fe270b798d8cb2dd9faeaeb1986ec1fd2
kawaalaxy <clementg0105@hotmail.fr>
2021-12-15 00:37:03 +0100

    pokédex up

--- commands/pokédex.js
+++ commands/pokédex.js
@@ -5,15 +5,16 @@ module.exports =
   description: 'donne un pokémon du pokédex',
   async execute(message)
   {
-    r = Math.random()*898;
-    r = r - (r%1) + 1;
+    r = Math.random()*151;
+    r = r - (r%1);
     fs.readFile('commands/pokedex_bibli.txt', 'utf8', (err, data) =>{
       if (err){
         console.error(err);
         return;
       }
       console.log(data);
-      message.channel.send(`Ton pokémon est :${data}`);
+      lignes = data.split(\n);
+      message.channel.send(`Ton pokémon est : #${r+1} ${lignes[r]}`);
     });
     console.log(process.cwd());
   }
