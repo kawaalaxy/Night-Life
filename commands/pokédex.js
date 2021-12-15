32d5b933546f57b371da3d2044628c3d9b3b584d
kawaalaxy <clementg0105@hotmail.fr>
2021-12-15 21:03:57 +0100

    pokédex up

--- commands/pokédex.js
+++ commands/pokédex.js
@@ -9,31 +9,31 @@ module.exports =
     {
       args[0] = 'all';
     }
-    if (args[0] == '1')
+    if (args[0] == '1' || args[0] == '1G' || args[0] == '1g')
     {
       taille = 151;
     }
-    else if (args[0] == '2')
+    else if (args[0] == '2'|| args[0] == '2G' || args[0] == '2g')
     {
       taille = 251;
     }
-    else if (args[0] == '3')
+    else if (args[0] == '3'|| args[0] == '3G' || args[0] == '3g')
     {
         taille = 386;
     }
-    else if (args[0] == '4')
+    else if (args[0] == '4'|| args[0] == '4G' || args[0] == '4g')
     {
         taille = 493;
     }
-    else if (args[0] == '5')
+    else if (args[0] == '5'|| args[0] == '5G' || args[0] == '5g')
     {
         taille = 649;
     }
-    else if (args[0] == '6')
+    else if (args[0] == '6'|| args[0] == '6G' || args[0] == '6g')
     {
         taille = 721;
     }
-    else if (args[0] == '7')
+    else if (args[0] == '7'|| args[0] == '7G' || args[0] == '7g')
     {
         taille = 807;
     }
