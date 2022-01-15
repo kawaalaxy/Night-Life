f4889e7dd0f518e3699ccb7dc0c6b9b0ec706605
kawaalaxy <32813468+kawaalaxy@users.noreply.github.com>
2022-01-15 22:35:38 +0100

    first_video n'était pas défini

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,21 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 var queue = [];
-first_video;
+var first_video =  {
+  type: 'video',
+  videoId: 'w0AOGeqOnFY',
+  url: 'https://youtube.com/watch?v=w0AOGeqOnFY',
+  title: 'The Coconut Song - (Da Coconut Nut)',
+  description: '',
+  image: 'https://i.ytimg.com/vi/w0AOGeqOnFY/hqdefault.jpg',
+  thumbnail: 'https://i.ytimg.com/vi/w0AOGeqOnFY/hqdefault.jpg',
+  seconds: 215,
+  timestamp: '3:35',
+  duration: { toString: [Function: toString], seconds: 215, timestamp: '3:35' },
+  ago: '11 years ago',
+  views: 142201471,
+  author: { name: 'Jeff Lau', url: 'https://youtube.com/user/J3FFL4U' }
+  };
 
 module.exports =
 {
