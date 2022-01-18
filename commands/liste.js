a5cd4fe3f8d1814d732bf0de2d445c136e50323f
kawaalaxy <clementg0105@hotmail.fr>
2022-01-18 18:56:42 +0100

    up liste pour cas liste vide

--- commands/liste.js
+++ commands/liste.js
@@ -4,10 +4,18 @@ module.exports =
   description: "affiche la liste d'attente",
   async execute(message, liste)
   {
-    result = []
-    for (var i = 0; i < liste.length; i++) {
-      result.push(liste[i].title);
+    if (liste = [])
+    {
+      message.channel.send("la liste est vide")
     }
-    message.channel.send(result);
+    else
+    {
+      result = []
+      for (var i = 0; i < liste.length; i++) {
+        result.push(liste[i].title);
+      }
+      message.channel.send(result);
+    }
+    }
   }
 }
