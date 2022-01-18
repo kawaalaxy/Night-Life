4a8be76738271ebe8f189d43b7e8b6ac8ad60668
kawaalaxy <32813468+kawaalaxy@users.noreply.github.com>
2022-01-18 14:38:38 +0100

    liste up

--- commands/liste.js
+++ commands/liste.js
@@ -4,6 +4,10 @@ module.exports =
   description: "affiche la liste d'attente",
   async execute(message, liste)
   {
+    result = []
+    for (var i = 0; i < liste.length; i++) {
+      result.push(liste[i].title);
+    }
     message.channel.send(liste);
   }
 }
